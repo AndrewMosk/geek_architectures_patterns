8b6d4dca4763e3f3c9d15ef8b9f1997b0a650805
AndrewMosk <MoskaletsAndrey@yandex.ru>
2021-12-06 15:57:34 +0300

    class cashe service for item

--- src/main/java/ru/geekbrains/shop/service/ItemServiceImplementationCache.java
+++ src/main/java/ru/geekbrains/shop/service/ItemServiceImplementationCache.java
@@ -1,2 +1,37 @@
-package ru.geekbrains.shop.service;public class ItemServiceImplementationCache {
+package ru.geekbrains.shop.service;
+
+import ru.geekbrains.storage.model.Item;
+import ru.geekbrains.storage.repository.ItemRepository;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+public class ItemServiceImplementationCache implements ItemService {
+
+    @Resource
+    private ItemRepository itemRepository;
+
+    private final HashMap<Long, Item> cache;
+
+    public ItemServiceImplementationCache() {
+        cache = new HashMap<>();
+    }
+
+    @Override
+    public Optional<Item> findItemById(Long id) {
+        if (cache.containsKey(id)) {
+            return Optional.ofNullable(cache.get(id));
+        }
+        Optional<Item> item = itemRepository.findById(id);
+        item.ifPresent(value -> cache.put(id, value));
+        return item;
+    }
+
+    @Override
+    public List<Item> getItems(String name, BigDecimal minPrice, BigDecimal maxPrice) {
+        return itemRepository.findAll();
+    }
 }
